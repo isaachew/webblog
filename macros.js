1f5dc9be158247580f471eea9a6194100fb57ed5
isaachew <isaachewks@gmail.com>
2020-04-07 11:35:12 +0800

    Added sign in button

--- macros.js
+++ macros.js
@@ -1,5 +1,5 @@
 function macro(x,y,z){
-	var z=z.replace(/\\e/g,"`").replace(/\\b/g,"\\")
+	var z=z.replace(/\\t/g,"`").replace(/\\b/g,"\\")
 	switch(y){
 		case "d":return "`"
 		case "age":return (new Date()-new Date(z.slice(4),z.slice(2,4)-1,z.slice(0,2)))/31556952000|0
@@ -7,9 +7,6 @@ function macro(x,y,z){
 			var ce=document.createElement("div")
 			ce.innerHTML=z.replace(/&lt;/g,"<").replace(/&amp;/g,"&")
 			return ce.outerHTML
-		case "icode":
-			var rc=z.replace(/&lt;/g,"<").replace(/&amp;/g,"&")
-			return "<iframe srcdoc=\""+rc.replace(/"/g,"&quot;")+"\">hi</iframe>"
 		case "mono":return "<pre>"+z+"</pre>"
 		
 	}
--- s.php
+++ s.php
@@ -1,6 +1,10 @@
 <?php
 $ha=hash("sha256",$_POST["hello"]);
+if($_GET["new"]){
+
+
+}
 header("HTTP/1.1 302 Found");
 header("Location: /");
-header("Set-Cookie: user=".$ha;
+header("Set-Cookie: user=".$ha);
 ?>
